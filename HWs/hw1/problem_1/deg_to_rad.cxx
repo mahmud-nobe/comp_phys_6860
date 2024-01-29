216e402b0d545815ade23dd03e93fdfad4e44193
Md Mahmudunnobe <mahmud.nobe@wayne.edu>
2024-01-29 21:41:58 +0000

    comment the codes

--- HWs/hw1/problem_1/deg_to_rad.cxx
+++ HWs/hw1/problem_1/deg_to_rad.cxx
@@ -1,20 +1,26 @@
+// include necessary libraries for basic functions.
 # include <iostream>
 # include <iomanip>
 # include <cmath>
 
 using namespace std;
 
+// declare global constants
 const double pi = 3.14159265359;
 
 int main(){
+	// declare variables
 	double deg;
 	double rad;
 	
-	
+	// taking inputs from user
 	cout << "Please give an angle in Degree: ";
 	cin >> deg;
 	
+	// calculate the angle in radians using the appropriate formula
 	rad = deg * pi / 180; 
+	
+	// show output with 2 digits after decimal
 	cout << "The angle in Radian is: " << fixed << setprecision(2) << rad << endl;
 	
 	return 0;
--- HWs/hw1/problem_2/projectile_dist.cxx
+++ HWs/hw1/problem_2/projectile_dist.cxx
@@ -1,18 +1,21 @@
+// include necessary libraries for basic functions.
 # include <iostream>
 # include <iomanip>
 # include <cmath>
 
 using namespace std;
 
+// declare global constants
 const double g = 9.81;
 const double pi = 3.14159265359;
 
 int main(){
+	// declare variables
 	double v_0;
 	double angle_deg, angle_rad;
 	double dist;
 	
-	
+	// taking inputs from user
 	cout << "Please provide the following information about the projectile:" << endl;
 	cout << "Initial Velocity (in meter/second): "; 
 	cin >> v_0;
@@ -20,9 +23,13 @@ int main(){
 	cout << "Angle with the ground (in degree, between 0 to 180): "; 
 	cin >> angle_deg;
 
-	
+	// calculate the angle in radians to use in sine function
 	angle_rad = angle_deg * pi / 180;
+	
+	// calculate the distance in radians using the appropriate formula
 	dist = v_0*v_0 * sin(2*angle_rad) / g; 
+	
+	// show output with 2 digits after decimal
 	cout << "The horizontal distance in meter is: " << fixed << setprecision(2) << dist << endl;
 	
 	return 0;
