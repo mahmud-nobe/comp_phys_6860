5d1d6d23218c782963032117135aad936e48c937
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2024-02-08 00:58:23 -0500

    random array generator

--- homeworks/hw3/problem1/bubble_sort.cxx
+++ homeworks/hw3/problem1/bubble_sort.cxx
@@ -1,56 +1,52 @@
 // include necessary libraries for basic functions.
 # include <iostream>
 # include <iomanip>
-# include <math.h>
+# include <cmath>
+# include <ctime>
 
 using namespace std;
 
 // declare global constants
 const double pi = M_PI;
 
-template <typename func> // using a class that can be used for different datatypes
-
-/* function for differentiation using brute force secant methods 
-for function f_x at the point x using the given stepsize.
-The smaller the stepsize, the better the estimate.
-*/
-double secant_diff(func f_x, double x, double stepsize){
-	double derivative = ( f_x(x+stepsize) - f_x(x) ) / stepsize;
-	return derivative;
+void swap(double n1, double n2){
+	double temp = n1;
+	n1 = n2;
+	n2 = temp;
 }
 
-// function for sin(x)
-double sinx(double x){
-	return sin(x);
-};
+void bubble_sort(int arr[], int arr_len){
+	for(int i=0; i<arr_len; i++){
+		arr[i] += 1;
+	}
+}
 
 int main(){
-	// declare variables
-	double x, stepsize;
-	double analytic_soln, numerical_soln;
-	// our point of interests
-	double inputs[5] = {0, pi/4, pi/2, 3*pi/4, pi};
+	// declare fixed array
+	int arr[20];
 	
-	// taking inputs from user
-	cout << "Please provide the stepsize, h: ";
-	cin >> stepsize;
+	srand(time(NULL)); // first seed value
 	
-	// loop through all our point of interest
-	for(int i=0; i<5; i++){
-		x = inputs[i];
-		
-		// analytically diff of sin(x) = cos(x)
-		analytic_soln = cos(x);
-		
-		// numerical soln using the secant_diff function
-		numerical_soln = secant_diff(sinx, x, stepsize);
-		
-		// Print the both answers and their relative error
-		cout << endl << "At x = " << x << endl;
-		cout << "Analytical soln: " << analytic_soln << endl;
-		cout << "Numerical Soln: " << numerical_soln << endl;
-		cout << "Relative Error: " << abs(analytic_soln - numerical_soln)/analytic_soln << endl;
+	for (int i = 0; i<20; i++){
+		arr[i] = 1 + rand()%100; // create random number between 1-100
 	}
+	
+	// print the unsorted array
+	cout << "The random unsorted array: " << endl;
+	for(int i=0; i<20; i++){
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	
+	bubble_sort(arr, 20);
+	
+	
+	// print the sorted array
+	cout << endl <<  "The sorted array: " << endl;
+	for(int i=0; i<20; i++){
+		cout << arr[i] << " ";
+	}
+	cout << endl;
 	
 	return 0;
 };
